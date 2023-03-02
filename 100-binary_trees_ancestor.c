dbf4a36a54f2c541fbedf1a34dc3bbf61f978d90
Bolexzy <boluoyewumi79@gmail.com>
2023-03-02 01:27:17 +0100

    Update file

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -10,12 +10,19 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
+	binary_tree_t *parent1, *parent2;
+
 	if (first && second)
 	{
-		if (first == second || first == second->parent)
+		if (first == second)
 			return ((binary_tree_t *) first);
 
-		return (binary_trees_ancestor(first->parent, second->parent));
+		parent1 = first->parent, parent2 = second->parent;
+		if (first == parent2 || !parent1 || (!parent1->parent && parent2))
+			return (binary_trees_ancestor(first, parent2));
+		else if (parent1 == second || !parent2 || (!parent2->parent && parent1))
+			return (binary_trees_ancestor(parent1, second));
+		return (binary_trees_ancestor(parent1, parent2));
 	}
 	return (NULL);
 }
