7b7369ea38c4a5a6ac45631c395a48abe05a5c51
Bolexzy <boluoyewumi79@gmail.com>
2023-03-09 19:43:04 +0100

    Update file: recode

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -10,13 +10,12 @@
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
-	if (tree != NULL)
-	{
-		if (tree->n == value)
-			return ((bst_t *)tree);
-		if (tree->n > value)
-			return (bst_search(tree->left, value));
-		return (bst_search(tree->right, value));
-	}
-	return (NULL);
+	if (tree == NULL)
+		return (NULL);
+
+	if (tree->n == value)
+		return ((bst_t *)tree);
+
+	return (tree->n > value
+			? bst_search(tree->left, value) : bst_search(tree->right, value));
 }
